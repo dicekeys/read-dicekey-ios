52251af3ffdf582e87b774fb74d6105a3aa20fb7
Nikita Titov <nmtitov@ya.ru>
2020-10-27 16:30:09 +0200

    Create Xcode framework template

---
+++ ReadDiceKey/ReadDiceKey/ReadDiceKey.h
@@ -0,0 +1,18 @@
+//
+//  ReadDiceKey.h
+//  ReadDiceKey
+//
+//  Created by Nikita Titov on 27.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReadDiceKey.
+FOUNDATION_EXPORT double ReadDiceKeyVersionNumber;
+
+//! Project version string for ReadDiceKey.
+FOUNDATION_EXPORT const unsigned char ReadDiceKeyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReadDiceKey/PublicHeader.h>
+
+
---
+++ ReadDiceKey/ReadDiceKeyTests/ReadDiceKeyTests.m
@@ -0,0 +1,36 @@
+//
+//  ReadDiceKeyTests.m
+//  ReadDiceKeyTests
+//
+//  Created by Nikita Titov on 27.10.2020.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ReadDiceKeyTests : XCTestCase
+
+@end
+
+@implementation ReadDiceKeyTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
