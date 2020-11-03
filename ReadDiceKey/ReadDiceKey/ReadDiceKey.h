35d9ae7ce0a602786e4f96ac810645457c1e5cc1
Nikita Titov <nmtitov@ya.ru>
2020-11-03 12:36:21 +0200

    Import C++ files

--- ReadDiceKey/ReadDiceKey/ReadDiceKey.h
+++ ReadDiceKey/ReadDiceKey/ReadDiceKey.h
@@ -15,4 +15,5 @@ FOUNDATION_EXPORT const unsigned char ReadDiceKeyVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ReadDiceKey/PublicHeader.h>
 
+#import "DKImageProcessor.h"
 
