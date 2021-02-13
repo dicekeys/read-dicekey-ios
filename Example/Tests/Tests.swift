89bff26c835a6a23c44fa6bba317cffed2695d7c
bakhtiyor <bakhtiyor.k@gmail.com>
2021-02-13 09:09:05 +0500

    Setup initial unit tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -14,15 +14,9 @@ class Tests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        let image = UIImage(named: "test", in: Bundle(for: type(of: self)), compatibleWith: nil)!
+        XCTAssertNotNil(image)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
 }
