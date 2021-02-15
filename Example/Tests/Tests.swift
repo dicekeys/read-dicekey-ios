74558b8774944c54b8ffefbb2229f19b222a3e8c
bakhtiyor <bakhtiyor.k@gmail.com>
2021-02-15 16:29:22 +0500

    Adding simple unit tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -16,6 +16,26 @@ class Tests: XCTestCase {
     func testExample() {
         let image = UIImage(named: "test", in: Bundle(for: type(of: self)), compatibleWith: nil)!
         XCTAssertNotNil(image)
+        
+        let w = image.cgImage!.width
+        let h = image.cgImage!.height
+
+        let bitmap = image.bitmap!
+
+        let wrapper = DKImageProcessor.create()!
+        XCTAssertNotNil(wrapper)
+        
+        let process = wrapper.process(bitmap, width: Int32(w), height: Int32(h))
+        XCTAssertFalse(process)
+        
+        let overladData = wrapper.overlay(bitmap, width: Int32(w), height: Int32(h))
+        XCTAssertNotNil(overladData)
+        
+        let augmentedData = wrapper.augmented(bitmap, width: Int32(w), height: Int32(h))
+        XCTAssertNotNil(augmentedData)
+        
+        let json = wrapper.json()
+        XCTAssertNotNil(json)
     }
     
     
